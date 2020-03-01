73129cca1434bdd46851cdc3c5fd98d9e80999ed
David Wise <github@darksidestory.com>
2020-03-01 18:22:40 -0500

    Basics implemented

--- StaticAbstraction/Environment.cs
+++ StaticAbstraction/Environment.cs
@@ -1,14 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections;
 
 namespace StaticAbstraction
 {
     public class StAbEnvironment : IEnvironment
     {
-        public StAbEnvironment()
-        {
-            //Environment.
-        }
+        public virtual string CommandLine => Environment.CommandLine;
+
+        public virtual string CurrentDirectory { get => Environment.CurrentDirectory; set => Environment.CurrentDirectory = value; }
+
+#if !NET40
+        public virtual int CurrentManagedThreadId => Environment.CurrentManagedThreadId;
+#endif
+
+        public virtual int ExitCode { get => Environment.ExitCode;  set => Environment.ExitCode = value; }
+
+        public virtual bool HasShutdownStarted => Environment.HasShutdownStarted;
+
+        public virtual bool Is64BitOperatingSystem => Environment.Is64BitOperatingSystem;
+
+        public virtual bool Is64BitProcess => Environment.Is64BitProcess;
+
+        public virtual string MachineName => Environment.MachineName;
+
+        public virtual string NewLine => Environment.NewLine;
+
+        public virtual OperatingSystem OSVersion => Environment.OSVersion;
+
+        public virtual int ProcessorCount => Environment.ProcessorCount;
+
+        public virtual string StackTrace => Environment.StackTrace;
+
+        public virtual string SystemDirectory => Environment.SystemDirectory;
+
+        public virtual int SystemPageSize => Environment.SystemPageSize;
+
+        public virtual int TickCount => Environment.TickCount;
+
+#if (NETCORE30 || NETCORE31)
+        public virtual long TickCount64 => Environment.TickCount64;
+#endif
+
+        public virtual string UserDomainName => Environment.UserDomainName;
+
+        public virtual bool UserInteractive => Environment.UserInteractive;
+
+        public virtual string UserName => Environment.UserName;
+
+        public virtual Version Version => Environment.Version;
+
+        public virtual long WorkingSet => Environment.WorkingSet;
+                
+        public virtual void Exit(int exitCode)=> Environment.Exit(exitCode);
+
+        public virtual string ExpandEnvironmentVariables(string name) => Environment.ExpandEnvironmentVariables(name);
+
+        public virtual void FailFast(string message) => Environment.FailFast(message);
+
+        public virtual void FailFast(string message, Exception exception) => Environment.FailFast(message, exception);
+
+        //public virtual void FailFast(string message, Exception exception, string errorMessage) => Environment.FailFast(message, exception, errorMessage);
+
+        public virtual string[] GetCommandLineArgs() => Environment.GetCommandLineArgs();
+
+
+        public virtual string GetEnvironmentVariable(string variable) => Environment.GetEnvironmentVariable(variable);
+
+        public virtual string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target) => Environment.GetEnvironmentVariable(variable, target);
+
+        public virtual IDictionary GetEnvironmentVariables() => Environment.GetEnvironmentVariables();
+
+        public virtual IDictionary GetEnvironmentVariables(EnvironmentVariableTarget target) => Environment.GetEnvironmentVariables(target);
+
+        public virtual string GetFolderPath(Environment.SpecialFolder folder) => Environment.GetFolderPath(folder);
+
+        public virtual string GetFolderPath(Environment.SpecialFolder folder, Environment.SpecialFolderOption option) => Environment.GetFolderPath(folder, option);
+
+        public virtual string[] GetLogicalDrives() => Environment.GetLogicalDrives();
+
+        public virtual void SetEnvironmentVariable(string variable, string value) => Environment.SetEnvironmentVariable(variable, value);
+
+        public virtual void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target) => Environment.SetEnvironmentVariable(variable, value, target);
     }
 }
---
+++ StaticAbstraction/Mocks/MockEnvironment.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace StaticAbstraction.Mocks
+{
+    public class MockEnvironment : IEnvironment
+    {
+        public virtual string CommandLine { get; set; }
+
+        public virtual string CurrentDirectory { get; set; }
+
+#if !NET40
+        public virtual int CurrentManagedThreadId { get; set; }
+#endif
+
+        public virtual int ExitCode { get; set; }
+
+        public virtual bool HasShutdownStarted { get; set; }
+
+        public virtual bool Is64BitOperatingSystem { get; set; }
+
+        public virtual bool Is64BitProcess { get; set; }
+
+        public virtual string MachineName { get; set; }
+
+        public virtual string NewLine { get; set; }
+
+        public virtual OperatingSystem OSVersion { get; set; }
+
+        public virtual int ProcessorCount { get; set; }
+
+        public virtual string StackTrace { get; set; }
+
+        public virtual string SystemDirectory { get; set; }
+
+        public virtual int SystemPageSize { get; set; }
+
+        public virtual int TickCount { get; set; }
+
+#if (NETCORE30 || NETCORE31)
+        public virtual long TickCount64 { get; set; }
+#endif
+
+        public virtual string UserDomainName { get; set; }
+
+        public virtual bool UserInteractive { get; set; }
+
+        public virtual string UserName { get; set; }
+
+        public virtual Version Version { get; set; }
+
+        public virtual long WorkingSet { get; set; }
+
+        public virtual void Exit(int exitCode) { }
+
+        public virtual string ExpandEnvironmentVariables(string name) => null;
+
+        public virtual void FailFast(string message) { }
+
+        public virtual void FailFast(string message, Exception exception) { }
+
+        public virtual string[] GetCommandLineArgs() => default(string[]);
+
+        public virtual string GetEnvironmentVariable(string variable) => null;
+
+        public virtual string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target) => null;
+
+        public virtual IDictionary GetEnvironmentVariables() => null;
+
+        public virtual IDictionary GetEnvironmentVariables(EnvironmentVariableTarget target) => null;
+
+        public virtual string GetFolderPath(Environment.SpecialFolder folder) => null;
+
+        public virtual string GetFolderPath(Environment.SpecialFolder folder, Environment.SpecialFolderOption option) => null;
+
+        public virtual string[] GetLogicalDrives() => null;
+
+        public virtual void SetEnvironmentVariable(string variable, string value) { }
+
+        public virtual void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target) { }
+    }
+}
