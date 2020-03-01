f6ae655c76f0522a7a561ff86d7ee8ecf41794bc
David Wise <github@darksidestory.com>
2020-03-01 15:31:00 -0500

    updated output from method analyzer

---
+++ StaticAbstraction/Environment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticAbstraction
+{
+    public class StAbEnvironment : IEnvironment
+    {
+        public StAbEnvironment()
+        {
+            //Environment.
+        }
+    }
+}
